3d5b60eac4004aa0220b8fb7069781893daa77ca
Aman Pal <aman.pal@tothenew.com>
2025-07-18 15:25:49 +0530

    first commit

---
+++ src/main/java/com/example/SpringBoog2/SpringBoog2Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoog2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoog2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoog2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBoog2/SpringBoog2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoog2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoog2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
