6146c58454757c5946119b0a3152e56e5e3b427f
Aman Pal <aman.pal@tothenew.com>
2025-07-18 15:40:56 +0530

    Added: Q2 solution

--- src/main/java/com/example/SpringBoog2/SpringBoog2Application.java
+++ src/main/java/com/example/SpringBoog2/SpringBoog2Application.java
@@ -1,13 +1,25 @@
 package com.example.SpringBoog2;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBoog2Application {
+public class SpringBoog2Application implements CommandLineRunner {
+
+	//Q2 Solution
+	@Value("${spring.datasource.url}")
+	String H2Url;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoog2Application.class, args);
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(H2Url);
 	}
 
 }
